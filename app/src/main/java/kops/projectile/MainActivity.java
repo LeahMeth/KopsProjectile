3fc19ff29d2614a8854103dc0da2224b18f59c14
Naomi Englard <naamibe@gmail.com>
2015-12-31 22:06:36 -0500

    added shared preferences

--- app/src/main/java/kops/projectile/MainActivity.java
+++ app/src/main/java/kops/projectile/MainActivity.java
@@ -1,16 +1,50 @@
 package kops.projectile;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText angleEditText;
+    private EditText velocityEditText;
+    private EditText timeEditText;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        angleEditText = (EditText) findViewById(R.id.angleET);
+        velocityEditText = (EditText) findViewById(R.id.velocityET);
+        timeEditText = (EditText) findViewById(R.id.timeET);
+
+        // creates preferences for sharing date
+        preferences = this.getSharedPreferences("DEFAULT" , MODE_PRIVATE);
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        angleEditText.setText(preferences.getString("ANGLE",""));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("ANGLE", angleEditText.getText().toString());
+        editor.apply();
+
     }
 
     @Override
